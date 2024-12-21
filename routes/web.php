d46073bcdea9dc89b1f22cdc4b18ff4e3545ebe1
Chavalit Koweerawong <chavalit.kow@gmail.com>
2024-12-21 16:17:00 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -121,17 +121,21 @@ Route::middleware(['auth'])->group(function () {
 // use Illuminate\Support\Facades\DB;
 
 Route::get('query/sql', function () {
-    // $products = DB::select("SELECT * FROM products");
-    $products = DB::select("SELECT * FROM products WHERE price > 100");
+    $products = DB::select("SELECT * FROM products");
+    // $products = DB::select("SELECT * FROM products WHERE price > 100");
     return view('query-test', compact('products'));
 });
 Route::get('query/builder', function () {
-    // $products = DB::table('products')->get();
-    $products = DB::table('products')->where('price', '>', 100)->get();
+    $products = DB::table('products')->get();
+    // $products = DB::table('products')->where('price', '>', 100)->get();
     return view('query-test', compact('products'));
 });
 Route::get('query/orm', function () {
-    // $products = Product::get();
-    $products = Product::where('price', '>', 100)->get();
+    $products = Product::get();
+    // $products = Product::where('price', '>', 100)->get();
     return view('query-test', compact('products'));
+});
+
+Route::get('barchart', function () {    
+    return view('barchart');
 });
